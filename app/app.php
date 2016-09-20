4961d068a2e2d77aa6c665c58b5d349f2b4b8265
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-19 19:30:50 -0700

    final functionality complete

--- app/app.php
+++ app/app.php
@@ -1,23 +1,22 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/DayOfWeek.php";
+    require_once __DIR__."/../src/NumberToWords.php";
     date_default_timezone_set('America/Los_Angeles');
-    // session_start();
-    // if (empty($_SESSION['list_of_albums'])) {
-    //     $_SESSION['list_of_albums'] = array();
-    // }
+
     $app = new Silex\Application();
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
+
     $app->get("/", function() use ($app) {
         return $app['twig']->render('home.html.twig');
     });
 
     $app->post("/result", function() use ($app) {
-        $newDayOfWeek = new DayOfWeek;
-        $day = $newDayOfWeek->getDayOfWeek($_POST['month'], $_POST['date'], $_POST['year']);
-        return $app['twig']->render('home.html.twig', array('day' => $day));
+        $newNumberToWord = new NumberToWords;
+        $result = $newNumberToWord->getWords($_POST['input_number']);
+        return $app['twig']->render('home.html.twig', array('result' => $result));
     });
 
     return $app;
--- tests/NumberToWordsTest.php
+++ tests/NumberToWordsTest.php
@@ -2,96 +2,57 @@
     require_once "src/NumberToWords.php";
     class NumberToWordsTest extends PHPUnit_Framework_TestCase
     {
-        function test_getSingleAndDouble_one_digit()
+        function test_singleAndDouble_one_digit()
         {
             //Arrange
             $test_NumberToWords = new NumberToWords;
             $number = 6;
             //Act
-            $output = $test_NumberToWords->getSingleAndDouble($number);
+            $output = $test_NumberToWords->singleAndDouble($number);
             //Assert
             $this->assertEquals("six ", $output);
         }
 
-        function test_getSingleAndDouble_two_digit()
+        function test_singleAndDouble_two_digit()
         {
             //Arrange
             $test_NumberToWords = new NumberToWords;
             $number = 23;
             //Act
-            $output = $test_NumberToWords->getSingleAndDouble($number);
+            $output = $test_NumberToWords->singleAndDouble($number);
             //Assert
             $this->assertEquals("twenty three ", $output);
         }
-        function test_getSingleAndDouble_multiple_of_ten()
+        function test_singleAndDouble_multiple_of_ten()
         {
             //Arrange
             $test_NumberToWords = new NumberToWords;
             $number = 30;
             //Act
-            $output = $test_NumberToWords->getSingleAndDouble($number);
+            $output = $test_NumberToWords->singleAndDouble($number);
             //Assert
             $this->assertEquals("thirty ", $output);
         }
 
-        function test_getThreeDigits()
+        function test_hundreds()
         {
             //Arrange
             $test_NumberToWords = new NumberToWords;
-            $number = 300;
+            $number = "000000008";
             //Act
-            $output = $test_NumberToWords->getThreeDigits($number);
+            $output = $test_NumberToWords->hundreds($number);
             //Assert
-            $this->assertEquals("three hundred ", $output);
+            $this->assertEquals("eight ", $output);
         }
 
-        function test_addThousandSuffix()
-        {
-            //Arrange
-            $test_NumberToWords = new NumberToWords;
-            $number = 111999;
-            //Act
-            $output = $test_NumberToWords->addThousandSuffix($number);
-            //Assert
-            $this->assertEquals("one hundred eleven thousand nine hundred ninety nine ", $output);
+        function test_numberParse(){
+            $test_numberParse = new NumberToWords;
+            $number = 833;
+
+            $output = $test_numberParse->numberParse($number);
+
+            $this->assertEquals("000000000000833", $output);
         }
-        function test_addMillionsSuffix()
-        {
-            //Arrange
-            $test_NumberToWords = new NumberToWords;
-            $number = 9111999;
-            //Act
-            $output = $test_NumberToWords->addMillionsSuffix($number);
-            //Assert
-            $this->assertEquals("nine million one hundred eleven thousand nine hundred ninety nine ", $output);
-        }
-        function test_addBillionsSuffix()
-        {
-            //Arrange
-            $test_NumberToWords = new NumberToWords;
-            $number = 29111999;
-            //Act
-            $output = $test_NumberToWords->addBillionsSuffix($number);
-            //Assert
-            $this->assertEquals("two hundred ninety one billion twenty nine million one hundred eleven thousand nine hundred ninety nine ", $output);
-        }
-
-
-
-        function test_addTrillionsSuffix()
-        {
-            //Arrange
-            $test_NumberToWords = new NumberToWords;
-            $number = 9111999;
-            //Act
-            $output = $test_NumberToWords->addTrillionsSuffix($number);
-            //Assert
-            $this->assertEquals("nine hundred eleven trillion nine hundred eleven billion nine million one hundred eleven thousand nine hundred ninety nine ", $output);
-        }
-
-
-
-
 
         // export PATH=$PATH:./vendor/bin first and then you will only have to run $ phpunit tests
     }
